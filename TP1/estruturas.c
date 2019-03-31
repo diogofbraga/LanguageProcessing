df3294886690903a7d8800d87771270d40249477
joaop21 <jpsilva9898@gmail.com>
2019-03-31 17:35:29 +0100

    documentação das estruturas

--- TP1/estruturas.c
+++ TP1/estruturas.c
@@ -18,8 +18,6 @@ Autor createAutor(char* novo_nome) {
 }
 
 void insertQuote(Autor au, char* quote){
-    /**char* word = "\&quot;";
-    deleteWord(quote,word);*/
     au->quotes = g_list_append(au->quotes, quote);
 }
 
@@ -56,47 +54,5 @@ void imprimeCitacoes(TodasCitacoes tc){
             a->quotes = g_list_next(a->quotes);
         }
         values = g_list_next(values);
-    }
-}
-
-//////////////////////////////////// Auxiliares ////////////////////////////////////
-
-
-
-void deleteWord(char * str, char * toRemove){
-    int i, j, stringLen, toRemoveLen;
-    int found;
-
-    stringLen   = strlen(str);      // Length of string
-    toRemoveLen = strlen(toRemove); // Length of word to remove
-
-
-    for(i=0; i <= stringLen - toRemoveLen; i++){
-        /* Match word with string */
-        found = 1;
-        for(j=0; j<toRemoveLen; j++){
-            if(str[i + j] != toRemove[j]){
-                found = 0;
-                break;
-            }
-        }
-
-        /* If it is not a word */
-        if(str[i + j] != ' ' && str[i + j] != '\t' && str[i + j] != '\n' && str[i + j] != '\0')
-            found = 0;
-
-        /*
-         * If word is found then shift all characters to left
-         * and decrement the string length
-         */
-        if(found == 1){
-            for(j=i; j<=stringLen - toRemoveLen; j++)
-                str[j] = str[j + toRemoveLen];
-
-            stringLen = stringLen - toRemoveLen;
-
-            // We will match next occurrence of word from current index.
-            i--;
-        }
     }
 }
--- TP1/estruturas.h
+++ TP1/estruturas.h
@@ -2,21 +2,49 @@
 #define ESTRUTURAS_H
 #endif
 
+
+/**
+@brief Estrutura que armazena o nome dum autor e a lista de citações que fez
+*/
 typedef struct Autor* Autor;
 
+/**
+@brief Estrutura que armazena uma Hash Table em que a chave é o nome do autor
+    e o valor é um estrutura do tipo Autor.
+*/
 typedef struct TodasCitacoes* TodasCitacoes;
 
+/**
+@brief Função que cria uma estrutura do tipo Autor
 
+@param novo_nome, É a string que contém um nome de autor
+
+@returns Autor, Autor criado
+*/
 Autor createAutor(char* novo_nome);
 
 
+/**
+@brief Função que cria uma estrutura do tipo TodasCitacoes
 
+@returns TodasCitacoes, Estrutura TodasCitacoes criada
+*/
 TodasCitacoes createTodasCitacoes();
 
+/**
+@brief Função que insere uma citação no respetivo autor.
+
+@param tc, Estrutra do tipo TodasCitacoes onde será incluída a citação.
+@param nome, Nome do autor a que pertence a citação.
+@param citacao, Citação a ser inserida na estrutura
+
+@returns TodasCitacoes, Estrutura TodasCitacoes criada
+*/
 void insereCitacao(TodasCitacoes tc, char* nome, char* citacao);
 
+/**
+@brief Função que imprime para ecrã a estrutura passada como parâmetro.
+
+@param tc, Estrutura do tipo TodasCitacoes a ser imprimida.
+*/
 void imprimeCitacoes(TodasCitacoes tc);
-
-
-
-void deleteWord(char* str, char* toRemove);
